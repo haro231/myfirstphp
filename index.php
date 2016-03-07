f119b21e936e708a8a02b0b516b922331b1c149e
unknown <1419172@RG43067.rgu.ac.uk>
2016-03-07 09:55:13 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1419172
+ * Date: 07/03/2016
+ * Time: 09:48
+ */
+$name = "Harry Asin";
+$age = 30;
+echo "My name is" . $name. "and I am" . $age
+
+    ?>
