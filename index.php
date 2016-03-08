ef50e99c5e8f940d2ca12631bdfc2b66b9743bc3
1419172 <1419172@rgu.ac.uk>
2016-03-08 10:27:28 +0000

    guy

--- index.php
+++ index.php
@@ -7,10 +7,9 @@
 <body>
 <?php
 // define variables and set to empty values
+$name = $age = "";
 
-
-/*
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+/*if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = test_input($_POST["name"]);
     $age = test_input($_POST["age"]);
 }
@@ -23,16 +22,42 @@ function test_input($data) {
 }
 */
 ?>
+
 <h2>
     SPECS, MUGS & SAUSAGES
 </h2>
-<form method ="post" action="process.php">
+<form method = "post" action ="index.php">
     Name: <input type = "text" name = "name">
-    Age: <input type = "number" name = "age">
+    Age: <input type = "number" age = "age">
     <input type = "submit" name = "submit" value=" Submit">
 </form>
 
+<?php
 
+//$name = $age = "";
+$name = $_POST['name'];
+$age = $_POST['age'];
+
+
+if ($age >= 21){
+    print "You can buy specs, mugs & sausages";
+}
+elseif($age >= 18 && $age < 21 ) {
+    print "You can buy specs & mugs";
+}
+elseif ($age >= 16 && $age < 18) {
+    print "You can buy only specs";
+}
+elseif ($age > 0 && $age < 16 ){
+    print "COME BACK WHEN YOU ARE 16 OR OVER!!!!!";
+}
+
+echo "<h2>Your Input: </h2>";
+echo $name;
+echo "<br>";
+echo $age;
+
+?>
 
 
 </body>
